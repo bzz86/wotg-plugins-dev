8676d8a113212b58746c2aadba920223258a1739
bzz86 <bzz86@mail.ru>
2014-10-04 23:53:30 +0200

    upd

--- BattleCardViewDev/BattleCardViewDev.js
+++ BattleCardViewDev/BattleCardViewDev.js
@@ -382,8 +382,8 @@ new Wotg.Plugins.Simple({
 
         drawNames: function method() {
             method.previous.apply(this, arguments);
-            this.objects.player.name.setText(Wotg.battle().player.name + (Wotg.battle().player.playerno + 1));
-            this.objects.opponent.name.setText(Wotg.battle().opponent.name + (Wotg.battle().opponent.playerno + 1));
+            this.objects.player.name.setText(Wotg.battle().player.name + "<br/> Ходит " +(Wotg.battle().player.playerno + 1));
+            this.objects.opponent.name.setText(Wotg.battle().opponent.name + "<br/> Ходит " + (Wotg.battle().opponent.playerno + 1));
         }
     });
 
